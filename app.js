b5b45876dec028bd247fb05bfff44479a1480d7d
gregpaes <gregpaes2@gmail.com>
2022-05-24 15:57:49 -0300

    Adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o Sitemas de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+console.log("O Paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o GitHub")
